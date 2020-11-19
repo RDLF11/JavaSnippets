8eab7c0e639afb2b8edb721f90b773df4a0ab374
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2020-11-19 12:46:40 +0100

    renamed scannerHelp and added char scanning

--- src/scannerHelp.java
+++ src/scannerHelp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class scannerClose {
+public class scannerHelp {
 
 	public static void scannerClosing() {
 		
@@ -19,8 +19,16 @@ public class scannerClose {
 		System.out.println("Closing Scanner...");
 		YourScannerName.close();
 		System.out.println("Scanner Closed.");
+
 	}
 
+		//to scan individual characters
+		
+		//https://www.geeksforgeeks.org/gfact-51-java-scanner-nextchar/
+		//Didn't know how to scan for a character
+		//input.next().charAt(0);
+
+		
 	public static void main(String[] args) {
 
 		scannerClosing();
